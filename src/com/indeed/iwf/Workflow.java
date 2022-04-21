d37721453e8d7e757eead41152224948b9d0fcfe
Quanzheng Long <qlong@indeed.com>
2022-04-21 10:39:20 -0700

    use interface

--- src/com/indeed/iwf/Workflow.java
+++ src/com/indeed/iwf/Workflow.java
@@ -15,77 +15,36 @@ import java.util.List;
  *
  * @param <I> the input type of the workflow
  */
-public class Workflow<I> {
+public interface Workflow<I> {
+    /**
+     * This input type of the workflow is required because SDK need to decode the data using dataConverter
+     */
+    Class<I> getInputType();
 
     /**
-     * This is the minimum to define a workflow
-     *
-     * @param inputType the class type of the input
-     * @param states    defines the states of the workflow. A state represents a step of the workflow state machine.
+     defines the states of the workflow. A state represents a step of the workflow state machine.
      *                  A state can execute some activities and wait for result, set timers to wait for firing, wait for signals.
      *                  Upon any of the conditions is met, a callback of the state will be executed to decide what to do next(wait for more conditions, or go to next state).
      *                  See more details in the state definition.
      */
-    public Workflow(final Class<I> inputType, final List<WorkflowState> states) {
-        this.inputType = inputType;
-        this.states = states;
-    }
+    List<WorkflowState> getStates();
 
     /**
-     * @param signalMethods register all the signal methods supported by this workflow.
+     * register all the signal methods supported by this workflow.
      */
-    public Workflow(final Class<I> inputType, final List<WorkflowState> states, final List<SignalMethod> signalMethods) {
-        new Workflow(inputType, states);
-        this.signalMethods = signalMethods;
-    }
+    List<SignalMethod> getSignalMethods();
 
     /**
-     * @param queryMethods defines all the query methods supported by this workflow.
+     * defines all the query methods supported by this workflow.
      *                     NOTE: different from Cadence/Temporal, a query method is not executed throw Cadence/Temporal workflow worker.
      *                     It won't run into non-deterministic issues, no matter how you modify it.
      */
-    public Workflow(final Class<I> inputType, final List<WorkflowState> states, final List<SignalMethod> signalMethods, final List<QueryMethod> queryMethods) {
-        new Workflow(inputType, states, signalMethods);
-        this.queryMethods = queryMethods;
-    }
+    List<QueryMethod> getQueryMethods();
 
     /**
      * Because the output type can be changed which causes data cannot be decoded to the new type.
      * Implement this handler is to fix this in-compatible change so that the data can still be decoded to the old type.
-     *
-     * @param handler the handler to handle the exception
      */
-    public Workflow(final Class<I> inputType, final List<WorkflowState> states, final List<SignalMethod> signalMethods, final List<QueryMethod> queryMethods, DecodingSourceDataExceptionHandler handler) {
-        new Workflow(inputType, states, signalMethods, queryMethods);
-        this.handler = handler;
-    }
-
-    // NOTE: This input type of the workflow is required because SDK need to decode the data using dataConverter
-    private Class<I> inputType;
-
-    private List<WorkflowState> states;
-    private List<SignalMethod> signalMethods;
-    private List<QueryMethod> queryMethods;
-    private DecodingSourceDataExceptionHandler handler;
-
-    Class<I> getInputType() {
-        return inputType;
-    }
-
-    public List<WorkflowState> getStates() {
-        return states;
-    }
-
-    public List<SignalMethod> getSignalMethods() {
-        return signalMethods;
-    }
-
-    public List<QueryMethod> getQueryMethods() {
-        return queryMethods;
-    }
-
-    public DecodingSourceDataExceptionHandler getDecodingSourceDataExceptionHandler() {
-        return handler;
-    }
+    DecodingSourceDataExceptionHandler getDecodingSourceDataExceptionHandler();
 }
 
---
+++ src/com/indeed/iwf/demo/SequenceActivityWorkflow.java
@@ -0,0 +1,4 @@
+package com.indeed.iwf.demo;
+
+public class SequenceActivityWorkflow {
+}
