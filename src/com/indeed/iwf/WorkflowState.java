ec1d8d3ef320fae44fa372d69a47f9b5895dd02e
Quanzheng Long <qlong@indeed.com>
2022-05-06 11:57:14 -0700

    Fix comment for wf state

--- src/com/indeed/iwf/WorkflowState.java
+++ src/com/indeed/iwf/WorkflowState.java
@@ -34,14 +34,21 @@ public interface WorkflowState<I> {
      * Implement this method to execute the commands set up for this state.
      *
      * @param input            the state input which is deserialized by dataConverter with {@link #getInputType}
-     * @param queryAttributes
-     * @param searchAttributes
+     * @param queryAttributes  the query attributes that can be used as readOnly
+     * @param searchAttributes the search attributes that can be used as readOnly
      * @return the requested commands for this step
+     * NOTE: it's readonly here for simplifying the implementation(execute can be reverted in some edge cases)
      */
     CommandRequest execute(I input, final SearchAttributesRO searchAttributes, final QueryAttributesRO queryAttributes);
 
     /**
      * Implement this method to decide what to do next when requested commands are ready
+     *
+     * @param input            the state input which is deserialized by dataConverter with {@link #getInputType}
+     * @param commandResults   the results of the command that executed by {@link #execute}
+     * @param queryAttributes  the query attributes that can be used as Read+Write
+     * @param searchAttributes the search attributes that can be used as Read+Write
+     * @return the decision of what to do next(e.g. transition to next states)
      */
     WorkflowStateDecision decide(final I input, final CommandResults commandResults,
                                  final SearchAttributesRW searchAttributes, final QueryAttributesRW queryAttributes);
